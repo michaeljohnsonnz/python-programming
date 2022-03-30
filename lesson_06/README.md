4c13bc4394ac6ae043d0dc3333c9926f9f92e920
Michael Johnson <michael.johnson@xtra.co.nz>
2022-03-30 14:29:25 +1300

    lesson 06

---
+++ lesson_06/README.md
@@ -0,0 +1,17 @@
+# Working with Composite Data Types
+
+## Lab overview
+
+A composite data type is any data type that comprises primitive data types. If you like food, you can visualize a composite data type as a turducken, which is a dish that consists of a chicken that is stuffed into a duck, which is stuffed into a turkey. In this lab, you will create a data type that consists of a string that is in a dictionary, which is in a list.
+
+In this lab, you will:
+
+- Use numeric data types
+- Use string data types
+- Use the dictionary data type
+- Use the list data type
+- Use a for loop
+- Use the print() function
+- Use the if statement
+- Use the else statement
+- Use the import statement
---
+++ lesson_06/car_inventory.py
@@ -0,0 +1,57 @@
+import csv
+import copy
+from os.path import exists
+
+myVehicle = {
+    "vin" : "<empty>",
+    "make" : "<empty>" ,
+    "model" : "<empty>" ,
+    "year" : 0,
+    "range" : 0,
+    "topSpeed" : 0,
+    "zeroSixty" : 0.0,
+    "mileage" : 0
+}
+
+for key, value in myVehicle.items():
+    print("{} : {}".format(key,value))
+
+myInventoryList = []
+
+# Added this to make this script work from the python-programming directory
+if exists("car_fleet.csv"):
+    file_name = "car_fleet.csv"
+elif exists("lesson_06/car_fleet.csv"):
+    file_name = "lesson_06/car_fleet.csv"
+else:
+    file_name = "NOT_FOUND"
+
+
+with open(file_name) as csvFile:
+    csvReader = csv.reader(csvFile, delimiter=',')  
+    lineCount = 0  
+    for row in csvReader:
+        if lineCount == 0:
+            print(f'Column names are: {", ".join(row)}')  
+            lineCount += 1  
+        else:  
+            print(f'vin: {row[0]} make: {row[1]}, model: {row[2]}, year: {row[3]}, range: {row[4]}, topSpeed: {row[5]}, zeroSixty: {row[6]}, mileage: {row[7]}')  
+            currentVehicle = copy.deepcopy(myVehicle)  
+            currentVehicle["vin"] = row[0]  
+            currentVehicle["make"] = row[1]  
+            currentVehicle["model"] = row[2]  
+            currentVehicle["year"] = row[3]  
+            currentVehicle["range"] = row[4]  
+            currentVehicle["topSpeed"] = row[5]  
+            currentVehicle["zeroSixty"] = row[6]  
+            currentVehicle["mileage"] = row[7]  
+            myInventoryList.append(currentVehicle)  
+            lineCount += 1  
+    print(f'Processed {lineCount} lines.')
+
+currentVehicle = copy.deepcopy(myVehicle)
+
+for myCarProperties in myInventoryList:
+    for key, value in myCarProperties.items():
+        print("{} : {}".format(key,value))
+        print("-----")
