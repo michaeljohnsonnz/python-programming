c91bced3ee9c02a580df34a31d468567571e20b6
Michael Johnson <michael.johnson@xtra.co.nz>
2022-04-02 15:12:52 +1300

    backup

--- z_experiment/test.py
+++
@@ -1,3 +0,0 @@
-from colors import Colors
-
-print(f"{Colors.GREEN}Success!{Colors.RESET} You've coloured your text in green")
---
+++ z_experiment/test_1.py
@@ -0,0 +1,5 @@
+import sounddevice as sd
+
+print(sd.query_devices())
+
+print(sd.query_hostapis())
